ac32ca19169ad91ed1c9f892d6b04a77bc86eca7
kaniadhea <kaniadhea7@gmail.com>
2023-06-19 15:41:12 +0700

    data pertama

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -7,5 +7,10 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan arrat integer 3 elemen
+		cout<<data.at
+	}
 }
