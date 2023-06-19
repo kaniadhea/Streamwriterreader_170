f11760ff544d7bfbb3511a1078ebabe90cfcfc61
kaniadhea <kaniadhea7@gmail.com>
2023-06-19 14:47:13 +0700

    membuka file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main() {
 	string baris;
+	
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjukan ke sebuah nama file 
+	outfile.open("contohfile.txt");
 
 }
